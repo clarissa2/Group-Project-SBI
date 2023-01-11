020e2b2c3529dd16be1bbc1e9f5892c5814d30a5
clarissa2 <clarissa.auckenthaler@gmail.com>
2023-01-11 11:40:01 +0100

    Update Methods bottom-up phase

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,4 @@
-import java.util.Dictionary;
+import java.util.*;
 
 public class Node {
     private Node left;
@@ -7,12 +7,16 @@ public class Node {
     private final int nodeID;
     private String name;
     private String characterStateWeight;
-    private String characterStateDental;
-    private int score;
     private double[] scores;
     private double S_low;
     private double S_high;
     private double S_unknown;
+    private String[] characterStates;
+    private ArrayList<double[]> parsimonyScores;
+
+    private double [] S_state1;//eg. [S_low,S_28,S_...]
+    private double [] S_state2;//eg. [S_high,S_30,S_...]
+    private double [] S_state3;//eg. [S_unknown,S_unknown,S_...]
 
     public Node(int nodeID) {
         this.nodeID = nodeID;
@@ -21,7 +25,7 @@ public class Node {
         this.parent = null;
         this.name = "";
         this.characterStateWeight = "";
-        this.characterStateDental ="";
+        this.characterStates = null;
     }
 
     public void addChild(Node node) {
@@ -58,102 +62,104 @@ public class Node {
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getCharacterStateWeight() {
-        return characterStateWeight;
+    public String getCharacterStates(int ind){
+        return this.characterStates[ind];
     }
 
-    public void setCharacterStateWeight(String characterState) {
-        this.characterStateWeight = characterState;
+    public void setCharacterStates(Dictionary<String,String[]> dictionary){
+        this.characterStates = dictionary.get(this.name);
     }
 
-    public String getCharacterStateDental() {
-        return characterStateDental;
+    public String[] getCharacterStates(){
+        return this.characterStates;
     }
 
-    public void setCharacterStateDetal(String characterState) {
-        this.characterStateDental = characterState;
+    public String getCharacterStatesAtIndex(int index){
+        return this.characterStates[index];
     }
 
-    public int getScore() {
-        return score;
+    public void setParsimonyScores(ArrayList<double[]> scores){
+       this.parsimonyScores= scores;
+
     }
 
-    public void setScore(int score) {
-        this.score = score;
+    public ArrayList<double[]> getParsimonyScores(){
+            return this.parsimonyScores;
+    }
+    public double[] getParsimonyScoresAtIndex(int index){
+        return this.parsimonyScores.get(index);
     }
 
-    public double[] getScores() {
-        return this.scores;
-    }
-    public double getScoresAtIndex(int index) {
-        return this.scores[index];
+    public void setS_state1AtIndex(double score, int index){
+        this.S_state1[index] = score;
     }
 
-    public void setScores(double[] scores) {
-        this.scores = scores;
+    public double[] getS_state1(){
+        return this.S_state1;
     }
 
-    public Node getParent() {
-        return parent;
+    public double getS_state1AtIndex(int index){
+        return this.S_state1[index];
     }
 
-    public void setParent(Node parent) {
-        this.parent = parent;
+    public void setS_state2AtIndex(double score, int index){
+        this.S_state2[index] = score;
     }
 
-    public int getNodeID() {
-        return nodeID;
+    public double[] getS_state2() {
+        return this.S_state2;
     }
 
-    public double getS_low() {
-        return S_low;
-    }
-    public double getS_high() {
-        return S_high;
-    }
-    public double getS_unknown() {
-        return S_unknown;
+    public double getS_state2AtIndex(double score, int index){
+        return this.S_state2[index];
     }
 
-    public void initScores(String[] characterStates, Dictionary dictionary) {
-        /**
-         * G
-         */
-        Double inf = Double.POSITIVE_INFINITY;
-        if(dictionary.get(this.name).equals(characterStates[0])){
-            double[] l = new double[]{0, inf, inf};
-            this.scores= l;
-        } else if (dictionary.get(this.name).equals(characterStates[1])) {
-            double[] h = new double[]{inf, 0, inf};
-            this.scores = h;
+    public void setS_state3AtIndex(double score, int index){
+        this.S_state3[index] = score;
+    }
 
-        } else {
-            double[] u = new double[]{inf, inf, 0};
-            this.scores = u;
+    public double[] getS_state3() {return this.S_state3;}
 
+    public double getS_state3AtIndex(double score, int index){return this.S_state3[index];}
+
+    //public String getCharacterStateWeight() {return characterStateWeight;}
+
+    //public void setCharacterStateWeight(String characterState) {this.characterStateWeight = characterState;}
+
+    public double[] getScores() {return this.scores;}
+
+    public double getScoresAtIndex(int index) {return this.scores[index];}
+
+    public void setScores(double[] scores) {this.scores = scores;}
+
+    public Node getParent() {return parent;}
+
+    public void setParent(Node parent) {this.parent = parent;}
+
+    public int getNodeID() {return nodeID;}
+
+    public double getS_low() {return S_low;}
+
+    public double getS_high() {return S_high;}
+
+    public double getS_unknown() {return S_unknown;}
+
+
+
+    public List<Node>  traversePreOrder(Node node, List<Node> list) {
+        if (node != null) {
+            list.add(node);
+            //System.out.println(node.name);
+            traversePreOrder(node.left,list);
+            traversePreOrder(node.right,list);
         }
+        return list;
     }
 
-    public void updateScores(Node childnode1, Node childnode2, double [][]weightMatrix){
-        /**
-         * Update the scores, using Sankoffs small parsimoies algorithm
-         */
-        Double inf = Double.POSITIVE_INFINITY;
-
-        double [] low_vectorChild1 =new double[]{(childnode1.getScoresAtIndex(0))+weightMatrix[0][0],childnode1.getScoresAtIndex(1)+weightMatrix[0][1],childnode1.getScoresAtIndex(2)+weightMatrix[0][2]} ;
-        double [] low_vectorChild2 =new double[]{(childnode2.getScoresAtIndex(0))+weightMatrix[0][0],childnode2.getScoresAtIndex(1)+weightMatrix[0][1],childnode2.getScoresAtIndex(2)+weightMatrix[0][2]};
-        this.S_low= (minimum(absolut(low_vectorChild1))+minimum(absolut(low_vectorChild2)));
 
 
-        double [] high_vectorChild1 =new double[]{(childnode1.getScoresAtIndex(0))+weightMatrix[1][0],childnode1.getScoresAtIndex(1)+weightMatrix[1][1],childnode1.getScoresAtIndex(2)+weightMatrix[1][2]} ;
-        double [] high_vectorChild2 =new double[]{(childnode2.getScoresAtIndex(0))+weightMatrix[1][0],childnode2.getScoresAtIndex(1)+weightMatrix[1][1],childnode2.getScoresAtIndex(2)+weightMatrix[1][2]};
-        this.S_high = (minimum(absolut(high_vectorChild1))+minimum(absolut(high_vectorChild2)));
 
-        this.S_unknown = inf;
 
-        this.scores= new double[]{S_low,S_high,S_unknown};
-    }
 
     //helper function to get minimum score in array
     public double minimum(double [] array) {
--- src/useOfSankoff_Spath_Auckenthaler.java
+++ src/useOfSankoff_Spath_Auckenthaler.java
@@ -1,10 +1,6 @@
 import java.io.IOException;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.Enumeration;
+
 /**
  * Project implementation for different uses of sankoff
  * Sequence Bioinformatics Group Project, WS 22/23
@@ -20,43 +16,43 @@ public class useOfSankoff_Spath_Auckenthaler {
      */
     public static void main(String[] args) throws IOException {
         System.out.println("useOfSankoff_Spath_Auckenthaler");
-        //initialize
-        String[] characterStates= new String[] {"low", "high", "unknown"};
-        String newick = "((Jaguarundi,Puma), Cheetah), Pallas))";
-        newick= newick.replace(" ","");
         Double inf = Double.POSITIVE_INFINITY;
 
+
+        //set values
+        String newick = "((Jaguarundi,Puma), Cheetah), Pallas))";
+        newick= newick.replace(" ","");
+        //sort of dollo-cost Matrix for 3 states
         double [][]weightMatrix = new double[][]{{0, 1, 1}, {1, 0, 1}, {inf, inf, 0}};
 
-        Dictionary<String, String> character_dic = new Hashtable<String, String>();
-        character_dic.put("Puma", "high");
-        character_dic.put("Jaguarundi", "low");
-        character_dic.put("Cheetah", "high");
-        character_dic.put("Pallas", "low");
+        //create dictionary of animals and their states
+        Dictionary<String, String[]> characters_dic = new Hashtable<String, String[]>();
+        String[] puma= new String[] {"high","30"};
+        String[] jaguarundi= new String[] {"low","30"};
+        String[] cheetah= new String[] {"high","30"};
+        String[] pallas= new String[] {"low","28"};
 
-        String[] DentalStates= new String[] {"28", "30", "unknown"};
-        double [][]dentalMatrix = new double[][]{{0, 1, 1}, {1, 0, 1}, {inf, inf, 0}};
-        Dictionary<String, String> characterDental_dic = new Hashtable<String, String>();
-        characterDental_dic.put("Puma", "30");
-        characterDental_dic.put("Jaguarundi", "30");
-        characterDental_dic.put("Cheetah", "30");
-        characterDental_dic.put("Pallas", "28");
+        characters_dic.put("Puma", puma);
+        characters_dic.put("Jaguarundi", jaguarundi);
+        characters_dic.put("Cheetah", cheetah);
+        characters_dic.put("Pallas", pallas);
 
+        String[][] states= new String[][]{{"low","high","unknown"},{"28","30","unknown"}};
 
-        //read in from argument
+        //read in from arguments
         if (args.length == 3) {
             newick = String.valueOf(args[0]);
-            //weightMatrix=;
-            //characterStates= ;
+
 
         }
-        //else {
-          //  throw new IOException("Usage: useOfSankoff_Spath_Auckenthaler [newick, matrix]");
-        //}
+        else {
+          //throw new IOException("Usage: useOfSankoff_Spath_Auckenthaler newick, matrix");
+        }
 
         System.out.println("NewickTree:"+newick);
-        //ArrayList<Node> nodes_newick = new ArrayList<Node>();
 
+        //make tree
+        //NEED TO BE CREATED BY NEWICK
         Node X = new Node(0);
         Node Pallas = new Node(1);
         Node Y = new Node(2);
@@ -68,77 +64,41 @@ public class useOfSankoff_Spath_Auckenthaler {
         X.setName("X");
         X.addChild(Pallas);
         X.addChild(Y);
-
         Pallas.setName("Pallas");
         Pallas.setParent(X);
-        Pallas.setCharacterStateDetal(characterDental_dic.get(Pallas.getName()));
-        Pallas.setCharacterStateWeight(character_dic.get(Pallas.getName()));
-        Pallas.initScores(characterStates,character_dic);
-
+        Pallas.setCharacterStates(characters_dic);
         Y.setName("Y");
         Y.addChild(Cheetah);
         Y.addChild(Z);
         Y.setParent(X);
-
         Cheetah.setName("Cheetah");
         Cheetah.setParent(Y);
-        Cheetah.setCharacterStateWeight(character_dic.get(Cheetah.getName()));
-        Cheetah.initScores(characterStates,character_dic);
-
-
+        Cheetah.setCharacterStates(characters_dic);
         Z.setName("Z");
         Z.setParent(Y);
         Z.addChild(Jaguarundi);
         Z.addChild(Puma);
-
         Puma.setName("Puma");
         Puma.setParent(Z);
-        Puma.setCharacterStateWeight(character_dic.get(Puma.getName()));
-        Puma.initScores(characterStates,character_dic);
-
+        Puma.setCharacterStates(characters_dic);
         Jaguarundi.setName("Jaguarundi");
         Jaguarundi.setParent(Z);
-        Jaguarundi.setCharacterStateWeight(character_dic.get(Jaguarundi.getName()));
-        Jaguarundi.initScores(characterStates,character_dic);
+        Jaguarundi.setCharacterStates(characters_dic);
+
+        //create top down and bottom up orderd List of Nodes
+        List<Node> list =new ArrayList<>();
+        list= X.traversePreOrder(X,list);
+        List<Node> newlist =new ArrayList<>();
+        newlist= reverseOrder(list);
 
 
-        Z.updateScores(Z.getLeftChild(),Z.getRightChild(),weightMatrix);
-        System.out.println("New Z-Scores:"+Arrays.toString(Z.getScores()));
-        System.out.println("New Puma-Scores:"+Arrays.toString(Puma.getScores()));
-        Y.updateScores(Y.getLeftChild(),Y.getRightChild(),weightMatrix);
-        System.out.println("New Y-Scores:"+Arrays.toString(Y.getScores()));
 
-        X.updateScores(X.getLeftChild(),X.getRightChild(),weightMatrix);
-        System.out.println("New X-Scores:"+Arrays.toString(X.getScores()));
-
-        String[] p1_low_high= new String[]{};
-        System.out.println(X.getRightChild().getCharacterStateWeight());
-        /**
-         *  int nodeID= 0;
-         *  boolean first = true;
-         *  Node currentNode = null;
-         * for (int i=0; i<newick.length() ;i++){
-         String c= Character.toString(newick.charAt(i));
-         if (first== true){
-         root= new Node (0);
-         currentNode = root;
-         first= false;
-         }
-         else if(c.equals("(")){
-         nodeID++;
-         node = new Node(nodeID);
-         node.setParent(currentNode);
-         currentNode.addChild(node);
-         currentNode= node;
-         }
-         else if (c.equals(",")){
-         if ()
-         }
-
-         }
-         */
-
-
+        //implement top-down phase of Sankoff's
+        //initalise states
+        initScores(newlist,states,characters_dic);
+        //Update states of Parent states
+        updateScores(newlist,weightMatrix);
+        
     }
 
 
@@ -156,5 +116,106 @@ public class useOfSankoff_Spath_Auckenthaler {
         double jc = intersection/unions;
         return jc;
     }
+    public static List<Node> reverseOrder(List<Node>list){
+        List<Node> newList = new ArrayList<>();
+        for (int i = list.size()-1; i >= 0; i--) {
+            newList.add(list.get(i));
+        }
+        return newList;
+    }
+
+    public static void initScores(List<Node> newlist, String[][] states, Dictionary dic) {
+        Double inf = Double.POSITIVE_INFINITY;
+        for(Node node:newlist){
+            System.out.println(node.getName());
+            String[] animalStates = node.getCharacterStates();
+            ArrayList<double[]> sc = new ArrayList<double[]>();
+            //init states
+            if(node.getCharacterStates()!= null){
+                for (int i=0; i<animalStates.length; i++){
+                    String [] cStates= states[i];
+                    double [] scores = new double[cStates.length];
+                    for(int j=0; j<cStates.length; j++){
+                        if(animalStates[i].equals(cStates[j])){
+                            scores[j] = 0;
+                        }
+                        else{
+                            scores[j]= inf;
+                        }
+                    }
+                    sc.add(scores);
+
+                    System.out.println("scores: " + Arrays.toString(scores));
+                }
+                node.setParsimonyScores(sc);
+            }
+
+        }
+    }
+
+    public static void updateScores(List<Node>newlist, double [][]weightMatrix){
+        /**
+         * Update the scores, using Sankoffs small parsimoies algorithm
+         */
+        for(Node node:newlist){
+            if (node.getParent()!= null) {
+                Node parent = node.getParent();
+                //System.out.println("parent: " + parent.getName());
+                Node childLeft = parent.getLeftChild();
+                //System.out.println("Left child: " + childLeft.getName());
+                Node childRight = parent.getRightChild();
+                //System.out.println("Right child: " + childRight.getName());
+                ArrayList<double[]> PS = new ArrayList<double[]>();
+
+                for (int k = 0; k < childLeft.getCharacterStates().length; k++) {
+                    double[] sL = childLeft.getParsimonyScoresAtIndex(k);
+                    //System.out.println("SL"+Arrays.toString(sL));
+                    double[] sR = childRight.getParsimonyScoresAtIndex(k);
+                    //System.out.println("SR"+Arrays.toString(sR));
+                    ArrayList<Double> low_vectorLeft = new ArrayList<Double>();
+                    ArrayList<Double> low_vectorRight = new ArrayList<Double>();
+                    ArrayList<Double> high_vectorLeft = new ArrayList<Double>();
+                    ArrayList<Double> high_vectorRight = new ArrayList<Double>();
+                    ArrayList<Double> unknown_vectorLeft = new ArrayList<Double>();
+                    ArrayList<Double> unknown_vectorRight = new ArrayList<Double>();
+
+                    low_vectorLeft.add(sL[0]+weightMatrix[0][0]);
+                    low_vectorLeft.add(sL[1]+weightMatrix[0][1]);
+                    low_vectorLeft.add(sL[2]+weightMatrix[0][2]);
+
+                    low_vectorRight.add(sR[0]+weightMatrix[0][0]);
+                    low_vectorRight.add(sR[1]+weightMatrix[0][1]);
+                    low_vectorRight.add(sR[2]+weightMatrix[0][2]);
+
+                    high_vectorLeft.add(sL[0]+weightMatrix[1][0]);
+                    high_vectorLeft.add(sL[1]+weightMatrix[1][1]);
+                    high_vectorLeft.add(sL[2]+weightMatrix[1][2]);
+
+                    high_vectorRight.add(sR[0]+weightMatrix[1][0]);
+                    high_vectorRight.add(sR[1]+weightMatrix[1][1]);
+                    high_vectorRight.add(sR[2]+weightMatrix[1][2]);
+
+                    unknown_vectorLeft.add(sL[0]+weightMatrix[2][0]);
+                    unknown_vectorLeft.add(sL[1]+weightMatrix[2][1]);
+                    unknown_vectorLeft.add(sL[2]+weightMatrix[2][2]);
+
+                    unknown_vectorRight.add(sR[0]+weightMatrix[2][0]);
+                    unknown_vectorRight.add(sR[1]+weightMatrix[2][1]);
+                    unknown_vectorRight.add(sR[2]+weightMatrix[2][2]);
+
+                    double[] S = new double[3];
+                    S[0] = ((Collections.min(low_vectorLeft) + Collections.min(low_vectorRight)));
+                    S[1] = ((Collections.min(high_vectorLeft) + Collections.min(high_vectorRight)));
+                    S[2] = ((Collections.min(unknown_vectorLeft) + Collections.min(unknown_vectorRight)));
+
+                    PS.add(S);
+                    //System.out.println("updated Scores: " + parent.getName() + " " + " " + Arrays.toString(S));
+
+                }
+                parent.setParsimonyScores(PS);
+            }
+        }
+    }
+
 
 }
