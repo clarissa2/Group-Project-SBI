694534703cea0efd76d281a583ba889901dd54bf
vinceaps <90271444+vinceaps@users.noreply.github.com>
2023-01-12 19:47:43 +0100

    newick parser

--- src/newickParser.java
+++ src/newickParser.java
@@ -4,6 +4,11 @@ public class newickParser {
     public static void main(String[] args) {
         Node test = parseNewick("(((Jaguarundi,Puma),Cheetah),Pallas)");
         System.out.println(test.getName());
+        List<Node> list =new ArrayList<>();
+        list= test.traversePreOrder(test,list);
+        for (Node x : list) {
+            System.out.println(x.getName());
+        }
     }
 
     public static Node parseNewick(String newick) {
@@ -12,16 +17,17 @@ public class newickParser {
 
         // Initialize some variables to keep track of the current node and its properties
         Node current = null;
+        Node root = null;
+        int ascii = 90;
 
         // Loop through the characters in the Newick string
         for (int i = 0; i < newick.length(); i++) {
             char c = newick.charAt(i);
-            char c1 = newick.charAt(i+1);
-
 
             // If the character is an opening parenthesis, create a new node and push it onto the stack
              if (c == '(') {
-                current = new Node("test");
+                current = new Node(Character.toString((char) ascii));
+                ascii--;
                 if (!stack.isEmpty()) {
                     stack.peek().addChild(current);
                 }
@@ -30,8 +36,7 @@ public class newickParser {
             }
             // If the character is a comma, set the current node's right child to the last node on the stack and push it back onto the stack
              else if (c == ',') {
-                 current = new Node("test");
-                 stack.peek().addChild(current);
+                 continue;
              }
             // If the character is a closing parenthesis pop node from stack
              else if (c == ')') {
@@ -44,13 +49,17 @@ public class newickParser {
                      name += newick.charAt(i);
                      i++;
                  }
+                 i--;
                  current = new Node(name);
                  stack.peek().addChild(current);
              }
+             if (!stack.isEmpty()) {
+                 root = stack.peek();
+             }
+
         }
 
         // The last node on the stack should be the root of the tree
-        Node root = stack.pop();
         return root;
     }
     }
